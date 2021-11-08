e28424f7a49e57fe19ea25dc1724942930516e4e
Bryan Jacobs <bryan.jacobs@outsystems.com>
2021-11-08 12:29:58 -0700

    add documentation and get the rest api actually working

--- src/main/java/com/example/springdisocexample/NonSpringMain.java
+++ src/main/java/com/example/springdisocexample/NonSpringMain.java
@@ -1,8 +1,19 @@
 package com.example.springdisocexample;
 
+import com.example.springdisocexample.repo.PersonRepo;
+import com.example.springdisocexample.rest.PersonRest;
+import com.example.springdisocexample.service.PersonService;
+
 public class NonSpringMain {
 
     public static void main(String[] args) {
+
+        // even though we don't have the spring container we can manually inject the dependencies
+        PersonRepo personRepo = new PersonRepo();
+
+        PersonService personService = new PersonService(personRepo);
+
+        PersonRest personRest = new PersonRest(personService);
 
     }
 
--- src/main/java/com/example/springdisocexample/rest/PersonRest.java
+++ src/main/java/com/example/springdisocexample/rest/PersonRest.java
@@ -3,20 +3,42 @@ package com.example.springdisocexample.rest;
 import com.example.springdisocexample.model.Person;
 import com.example.springdisocexample.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * This creates a simple rest api using spring boot
+ *
+ * The @RestController creates an endpoint that is accessible from:
+ * http://localhost:8080/person/{id}
+ *
+ * Run the:
+ * SpringDisocExampleApplication from the build or your IDE
+ */
 @RestController
+@RequestMapping(path="person")
 public class PersonRest {
-
 
     private PersonService personService;
 
-    @Autowired
+    @Autowired // this will cause the spring framework to inject the PersonService
     public PersonRest(PersonService personService) {
         this.personService = personService;
     }
 
-    public Person getPersonById(Integer id) {
+    /**
+     * once the application is running you can invoke this method over http from your browser like this:
+     *
+     * http://localhost:8080/person/1
+     *
+     * and you will get a json response
+     * @param id
+     * @return
+     */
+    @GetMapping(path = "/{id}")
+    public Person getPersonById(@PathVariable Integer id) {
         return personService.getById(id);
     }
 
