21cd3a0d5129441eeb46ffc528961416cc8b0fd8
Bryan Jacobs <bryan.jacobs@outsystems.com>
2021-11-08 12:33:48 -0700

    more documentation

--- src/main/java/com/example/springdisocexample/SpringDisocExampleApplication.java
+++ src/main/java/com/example/springdisocexample/SpringDisocExampleApplication.java
@@ -3,6 +3,15 @@ package com.example.springdisocexample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * this class is the main method for the spring boot application
+ * if you run this from your ide or from the maven build it will start a rest endpoint
+ * that is accessible from the browser.
+ *
+ * It has a single endpoint that can be invoked like this:
+ *
+ * http://localhost:8080/person/1
+ */
 @SpringBootApplication
 public class SpringDisocExampleApplication {
 
