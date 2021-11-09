2ea43a8d8097453a66e28a0d9939db4cd780d2a4
Bryan Jacobs <bryan.jacobs@outsystems.com>
2021-11-09 07:04:04 -0700

    add @Autowired even though it does not need to be there....that much be a new feature

--- src/main/java/com/example/springdisocexample/service/PersonService.java
+++ src/main/java/com/example/springdisocexample/service/PersonService.java
@@ -2,6 +2,7 @@ package com.example.springdisocexample.service;
 
 import com.example.springdisocexample.model.Person;
 import com.example.springdisocexample.repo.PersonRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -14,13 +15,12 @@ public class PersonService {
     @Value("${us.drinking.age}")
     private Integer usDrinkingAge;
 
-
     private PersonRepo personRepo;
 
+    @Autowired
     public PersonService(PersonRepo personRepo) {
         this.personRepo = personRepo;
     }
-
 
     public Person getById(Integer id) {
 
@@ -28,8 +28,9 @@ public class PersonService {
     }
 
     public List<Person> findLegalDrinkers() {
+
         List<Person> people = personRepo.getAll();
+
         return people.stream().filter(person -> person.getAge() >= usDrinkingAge).collect(Collectors.toList());
     }
-
 }
